57679d20d72e3a99264107ecc8e5600dc546a68c
Zach Nehrenberg <zachnehrenberg@hotmail.com>
2013-11-24 10:54:49 -0800

    I just want it to look like I'm popular on github

--- public/js/sidemenu.js
+++ public/js/sidemenu.js
@@ -1,8 +1,10 @@
+    var prompt = 0;
     $('.changeButton2').click(function () {
       $('#alert').addClass('in'); // shows alert with Bootstrap CSS3 implem
     });
     
     $('.close').click(function () {
+        prompt = 0;
       $(this).parent().removeClass('in'); // hides alert with Bootstrap CSS3 implem
     });
 
@@ -12,17 +14,30 @@
     });
     
     $('.close').click(function () {
+        prompt = 0;
       $(this).parent().removeClass('in'); // hides alert with Bootstrap CSS3 implem
     });
 
 
     $('#the-thing-that-opens-your-alert').click(function(){
-        var num = Math.ceil(Math.random() * 2);
-        if ( num == 1) {
-            $('#alert').addClass('in');
+        if (prompt == 0){
+            var num = Math.ceil(Math.random() * 2);
+            if ( num == 1) {
+                $('#alert').addClass('in');
+                prompt = 2;
+            }
+            else if (num == 2) {
+                $('#alert2').addClass('in');
+                prompt = 1;
+            }
         }
-        else if (num == 2) {
-            $('#alert2').addClass('in');
+        else {
+            if ( prompt == 1) {
+                $('#alert').addClass('in');
+            }
+            else if (prompt == 2) {
+                $('#alert2').addClass('in');
+            }
         }
     });
 
