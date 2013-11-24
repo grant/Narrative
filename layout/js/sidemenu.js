e8b45835f0fdbdd70941f608c52f68b9f0ae8f92
Zach Nehrenberg <zachnehrenberg@hotmail.com>
2013-11-23 19:44:18 -0800

    jhcggvhjkn

---
+++ layout/js/sidemenu.js
@@ -0,0 +1,8 @@
+  $("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
